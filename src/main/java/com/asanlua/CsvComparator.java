0a47333bbad81dcb83185903d57cc7babe07b11e
asanlua <mail@asanlua.com>
2021-11-04 10:10:54 +0100

    Update CsvComparator.java
    Avoid errors while comparison.

--- src/main/java/com/asanlua/CsvComparator.java
+++ src/main/java/com/asanlua/CsvComparator.java
@@ -6,9 +6,12 @@ public class CsvComparator implements Comparator<ParsedCsvElement> {
 
     @Override
     public int compare(ParsedCsvElement element1, ParsedCsvElement element2) {
-        if (element1.getValue() >= element2.getValue()) {
+        if (element1.getValue() > element2.getValue()) {
             return 1;
         }
-        return -1;
+        if (element1.getValue() < element2.getValue()) {
+            return -1;
+        }
+        return 0;
     }
 }
